de6017f4947bf4b2ff16c62cc2cd6ab14a1cecf9
Sergiostef25 <103273506+Sergiostef25@users.noreply.github.com>
2022-05-31 22:05:42 +0200

    Update attack_eval.py

--- OpenAttack/attack_eval/attack_eval.py
+++ OpenAttack/attack_eval/attack_eval.py
@@ -259,10 +259,10 @@ class AttackEval:
             
         df = pd.DataFrame(x_att, columns=['x_adv'])
         
-        #with open('/content/drive/MyDrive/datasets/adv_clothing.csv', 'a') as f:
-            #df.to_csv(f, header=False)
-        adv = [summary,df]
-        return adv
+        with open('/content/drive/MyDrive/TextAdvAtt/adv_clothing.csv', 'a') as f:
+            df.to_csv(f, header=False)
+        
+        return summary
     
     ## TODO generate adversarial samples
     
